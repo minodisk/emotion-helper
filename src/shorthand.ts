20fd2e7ee606ef7a7b3e1351f9d5e2ecb0657b80
Daisuke Mino <daisuke.mino@gmail.com>
2019-08-22 10:21:43 +0900

    Add sizes

--- src/shorthand.ts
+++ src/shorthand.ts
@@ -1,6 +1,11 @@
 import { MarginProperty, PaddingProperty } from "csstype";
+import { px } from "./unit";
 
-export const margin = (...sizes: Array<MarginProperty<string | 0>>) =>
-  Array.prototype.join.call(sizes, " ");
-export const padding = (...sizes: Array<PaddingProperty<string | 0>>) =>
-  Array.prototype.join.call(sizes, " ");
+export const sizes = (
+  ...sizes:
+    | Array<MarginProperty<string | 0>>
+    | Array<PaddingProperty<string | 0>>
+): string =>
+  sizes
+    .map(size => (typeof size === "number" && size !== 0 ? px(size) : size))
+    .join(" ");
